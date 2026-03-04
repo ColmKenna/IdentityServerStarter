5da9e236f76092beb365283e08046f88df46f930
ColmKenna <colmkenna@gmail.com>
2026-03-04 20:15:31 +0000

    refactor(task-extensions): convert Then method to async for improved performance and cancellation support

--- src/PrimativeExtensions/TaskExtensions.cs
+++ src/PrimativeExtensions/TaskExtensions.cs
@@ -9,8 +9,12 @@ public static class TaskExtensions
     
     public static Task<T> AsTask<T>(this T value) => Task.FromResult(value);
     
-    public static Task<R> Then<T, R>(this Task<T> task, Func<T, R> selector, CancellationToken cancellationToken = default)
-        => task.ContinueWith(t => selector(t.Result), cancellationToken);
+    public static async Task<R> Then<T, R>(this Task<T> task, Func<T, R> selector, CancellationToken cancellationToken = default)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+        var result = await task.ConfigureAwait(false);
+        return selector(result);
+    }
 }
 
 
